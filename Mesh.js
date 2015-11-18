9f7cea59279f16274f0c1faad018488e7885f9ce
Kathrine Hewlett <katt3985@gmail.com>
2015-11-18 15:54:17 -0600

    adding shaders and functions for decoding meshes

--- Mesh.js
+++ Mesh.js
@@ -19,3 +19,92 @@
         }
         return ret;
     }
+    
+    Mesh.prototype.decodePly = function(text)
+    {
+    var textByLine=text.split("\n");
+    var facesCount;
+    var verticesCount;
+    ReturnMesh = new Mesh();
+    if (textByLine[0].toLowerCase()=="ply")
+    {
+        //window.alert("appears to be a complete ply file");
+        var i=0;
+        while(textByLine[i] != "end_header")
+        {
+            var line = textByLine[i].split(" ");
+            if (line[0].toLowerCase() == "element")
+            {
+                if (line[1].toLowerCase() == "vertex")
+                {
+                    verticesCount = parseInt(line[2]);
+                }
+                if (line[1].toLowerCase() == "face")
+                {
+                    facesCount = parseInt(line[2]);
+                }
+            }
+        }
+        
+        i++
+        var j =i;
+        for(;i<verticesCount+j;j++)
+        {
+            var line = textByLine[i].split(" ");
+            ReturnMesh.vertices.push( vec3(
+                parseFloat( line[0] ),
+                parseFloat( line[1] ),
+                parseFloat( line[2] )
+            ));
+            ReturnMesh.normals.push( vec3(
+                parseFloat( line[3] ),
+                parseFloat( line[4] ),
+                parseFloat( line[5] )
+            ));
+
+        }
+        var pos = i
+        for(;i < facesCount+pos;i++)
+        {
+            var line = textByLine[i].split(" ");
+            ReturnMesh.faces.push( vec3(
+                parseInt(line[1]),
+                parseInt(line[2]),
+                parseInt(line[3])
+            ));
+        }
+    }
+    else
+    {
+        facesCount = parseInt(textByLine[1]);
+        verticesCount = parseInt(textByLine[0]);
+        var j =2;
+        for(;j<verticesCount+2;j++)
+        {
+            var line = textByLine[j].split(" ");
+            ReturnMesh.vertices.push( vec3(
+                parseFloat( line[0] ),
+                parseFloat( line[1] ),
+                parseFloat( line[2] )
+            ));
+            ReturnMesh.normals.push( vec3(
+                parseFloat( line[3] ),
+                parseFloat( line[4] ),
+                parseFloat( line[5] )
+            ));
+
+        }
+        var pos = j
+        for(;j < facesCount+pos;j++)
+        {
+            var line = textByLine[j].split(" ");
+            ReturnMesh.faces.push( vec3(
+                parseInt(line[1]),
+                parseInt(line[2]),
+                parseInt(line[3])
+            ));
+        }
+    }
+    
+    return ReturnMesh;
+}
--- main.js
+++ main.js
@@ -1,1 +1,27 @@
- 
+var gl;
+var program;
+
+window.onload = function init () 
+{
+    console.log("INIT:");
+    currentMesh = new Mesh();
+    //startup sequence
+    var canvas = document.getElementById("gl-canvas");
+    //makes a GL instance for our canvas
+    gl=WebGLUtils.setupWebGL(canvas);
+    //Enables Z-buffer depth testing
+    gl.enable(gl.DEPTH_TEST)
+    
+    //gets the shader programs from the HTML file
+    //and initializes our vertex and fragment shaders
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    //sets the resulting shader program to use
+    gl.useProgram(program);
+    
+    
+    //use black for the defaault color
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    
+}
