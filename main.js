d7992d3170c7e32d7805553da92ece6ba803086c
Kathrine Hewlett <katt3985@gmail.com>
2015-12-01 09:41:01 -0600

    moved redering code to the mesh, this way multiple meshes can be handeled better added the f16 mesh because the origional airplane mesh was way too big fixed rendering so that models are correctly added some primitive diffuse lighting

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var program;
 var PlayerMesh;
 var pointBuff;
 var normBuff;
-
+var theta;
+var aspect;
 window.onload = function init () 
 {
     console.log("INIT:");
@@ -24,10 +25,10 @@ window.onload = function init ()
     //use black for the defaault color
     gl.clearColor(0.0,0.0,0.0,1.0);
     
-    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.viewport(0, 0,canvas.width, canvas.height);
     
     
-    PlayerMesh = new Mesh();
+    PlayerMesh = new Mesh(gl);
     
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() 
@@ -37,23 +38,25 @@ window.onload = function init ()
             var text = xhttp.responseText;
             //window.alert(text);
             PlayerMesh.decodePly(text);
-            //window.alert("Mesh has "+ PlayerMesh.vertices.length + " vertices" );
+            window.alert("Mesh has "+ PlayerMesh.vertices.length + " vertices" );
+            PlayerMesh.loadToGPU();
             
-            pointBuff = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, pointBuff);
-            gl.bufferData(gl.ARRAY_BUFFER, flatten(PlayerMesh.vertices), gl.STATIC_DRAW);
-	
-            normBuff = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, normBuff);
-            gl.bufferData(gl.ARRAY_BUFFER, flatten(PlayerMesh.normals), gl.STATIC_DRAW);
+
         }
     };
-    xhttp.open("GET", "airplane.ply", true);
+    xhttp.open("GET", "f16.ply", true);
     xhttp.send();
+    theta=0;
+    aspect = canvas.clientWidth / canvas.clientHeight;
+
+    //
+    
+    
+    
     
     render();
 }
-
+/*
 function setupAttributes()
 {
     //if there are points in the point list...
@@ -78,69 +81,38 @@ function setupAttributes()
 	    gl.enableVertexAttribArray(vNormal);
 	}
 }
-
+*/
 function render()
 {
 
-
     //request refreshed frame for animation
     requestAnimFrame(render);
-
+    
+    
     //clears screen and depth buffers
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+
     //increases angle of rotation by half a degree each frame
-    //theta+=0.5;
+    //updateRotation();
 
-    //this will be our model view matrix
-    var modelMat=mat4();
-    //sets the camera at the origin, facing down the z-axis, with the up vector along the y-axis
-    modelMat=lookAt(vec3(0, 0, 0), vec3(0, 0, 1), vec3(0, 1, 0));
+          
+        //this will be out projection matrix
+        var projMat=mat4();
     
-    //scales geometry
-    modelMat=mult(modelMat, scalem(vec3(2.0, 2.0, 2.0)));
-    //translates geometry
-    modelMat=mult(modelMat, translate(vec3(0.0, 0.0, 5.0)));
-    //rotates geometry
-    modelMat=mult(modelMat, rotate(90.0, vec3(1.0, 0.0, 0.0)));
-    modelMat=mult(modelMat, rotate(190.0, vec3(0.0, 1.0, 0.0)));
-    modelMat=mult(modelMat, rotate(12.0, vec3(0.0, 0.0, 1.0)));
+        //sets up a projection with a 45 degree field of view,
+        //apsect ratio, near clipping plane of 0.1 and far of 1000.0
+        projMat=mult(projMat, perspective(45.0, aspect, 0.1, 1000.0));
+        
+        //projMat=mult(projMat, rotate(theta, vec3(0.0, 0.0, 0.0)));
+        theta += .5;
 
-    //this will be out projection matrix
-    var projMat=mat4();
-    
-    //sets up a projection with a 45 degree field of view,
-    //apsect ratio, near clipping plane of 0.1 and far of 1000.0
-    projMat=mult(projMat, perspective(45.0, 1.0, 0.1, 1000.0));
+  
+        var projMatGPtr=gl.getUniformLocation(program, "vProjMat");
+        gl.uniformMatrix4fv(projMatGPtr, false, flatten(projMat));
 
-    //gets location of the vRotMat uniform on the GPU
-    var modelMatLoc=gl.getUniformLocation(program, "vRotMat");
-
-    var colorLoc=gl.getUniformLocation(program, "vColor");
-
-    //gets location of the vRotMat uniform on the GPU
-    var projMatLoc=gl.getUniformLocation(program, "vProjMat");
-
-    //Sends the rotation matrix to the GPU as a uniform
-    gl.uniformMatrix4fv(modelMatLoc, false, flatten(modelMat));
-
-    //Sends the rotation matrix to the GPU as a uniform
-    gl.uniformMatrix4fv(projMatLoc, false, flatten(projMat));
-
-    //sets up attributes
-    setupAttributes()
-
-    //Sends color to the GPU as a uniform
-    gl.uniform4fv(colorLoc, flatten( vec4(1.0, 1.0, 1.0, 1.0) ));
-    gl.bindBuffer(gl.ARRAY_BUFFER, pointBuff);
-    //draws point list array as Triangles
-    gl.drawArrays(gl.TRIANGLES, 0, PlayerMesh.vertices.length);
-
-    //Sends new color to the GPU
-    gl.uniform4fv(colorLoc, flatten( vec4(0.0, 1.0, 0.0,1.0)));
-    //draws point list array as Lines
-    for(var i = 0; i < PlayerMesh.vertices.length; i+=3)
-    {
-    	gl.drawArrays(gl.LINE_LOOP, i, 3);
-    }
+        if (PlayerMesh.isLoaded)
+            PlayerMesh.render(theta);
+        
+        
 }
