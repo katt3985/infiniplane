964afc4edf296e4d5e84193ca24ff6a3625b8dac
John Cooper Lawton <JCLawton@hst000150946um.wifi03.olemiss.edu>
2015-12-01 19:29:55 -0600

    s://github.com/katt3985/infiniplane.git

--- TerrainMesh.js
+++ TerrainMesh.js
@@ -1,3 +1,4 @@
+var arrayZ = [];
 
     var FlatTerrainMesh = function(glPlease,or,sc,of,X,Y)
     {
@@ -14,6 +15,36 @@
             for(var j=0; j<this.sizeX;j++)
             {
                 this.vertices.push(vec3(j,0,i));
+                if(i<this.sizeY-1 && j<this.sizeX-1)
+                {
+                    this.faces.push(vec3( this.vertices.length -1, this.vertices.length, this.vertices.length-1+this.sizeX ));
+                    this.faces.push(vec3( this.vertices.length, this.vertices.length-1+this.sizeX, this.vertices.length + this.sizeX));
+                }
+            }
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    var diamondSquareFlatTerrainMesh = function(glPlease,or,sc,of,X,Y)
+    {
+        Mesh.apply(this,arguments);
+        this.sizeX =X;
+        this.sizeY =Y;
+    }
+    diamondSquareFlatTerrainMesh.prototype = new Mesh();
+    
+    diamondSquareFlatTerrainMesh.prototype.generate = function()
+    {
+        for(var i=0; i< this.sizeY;i++)
+        {
+            for(var j=0; j<this.sizeX;j++)
+            {
+                this.vertices.push(vec3(j, diamondSquare.pointList[i],i));
                 if(i<this.sizeY-1 && j<this.sizeX-1)
                 {
                     this.faces.push(vec3( this.vertices.length -1, this.vertices.length, this.vertices.length-1+this.sizeX ));
